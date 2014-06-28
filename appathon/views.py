5a55f05e3110acbf1261496272da6b8092e14ed5
greg.roberts@agilex.com <greg.roberts@agilex.com>
2014-06-27 20:55:25 -0400

    Updated device registration

--- appathon/views.py
+++ appathon/views.py
@@ -3,21 +3,32 @@ from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import simplejson
 from push_notifications.models import APNSDevice, GCMDevice
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import SiteProfileNotAvailable, User
 
 @csrf_exempt
 @require_POST
+@login_required
 def register_device(request, device_type):
     if request.method == 'POST':
         json_data = simplejson.loads(request.body)
         if device_type == 'gcm':
-            device = GCMDevice()
+            existingDevices = GCMDevice.objects.filter(registration_id=json_data['registration_id'])
+            if existingDevices:
+                device = existingDevices[0]
+            else:
+                device = GCMDevice()
         elif device_type == 'apns':
-            device = APNSDevice()
+            existingDevices = APNSDevice.objects.filter(registration_id=json_data['registration_id'])
+            if existingDevices:
+                device = existingDevices[0]
+            else:
+                device = APNSDevice()
         else:
             return HttpResponseBadRequest('Invalid device type')
-        device.name = "test"
-        device.registration_id = "abcd"
+        device.name = request.user.username
+        device.user = request.user
+        device.registration_id = json_data['registration_id']
         device.save()
-        print 'Raw Data: "%s"' % json_data['test']
 
     return HttpResponse("OK")
